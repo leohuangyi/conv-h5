c372ab24d858a73133c9e679d23dbfe1cfd7f1f8
xitang.hy <xitang.hy@alibaba-inc.com>
2016-04-11 17:05:28 +0800

    项目初始化

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp');
+var gulpConcat = require('gulp-concat');
+var gRename = require('gulp-rename');
+var del = require('del');
+var packageConfig = require('./package.json');
+
+gulp.task('build', ['clean', 'css', 'js']);
+
+gulp.task('clean', function(){
+    del('./dist/*');
+});
+gulp.task('js', function(){
+    gulp.src('./src/libs/Swiper/dist/js/*.min.js')
+        .pipe(gRename(function(path){
+            var basename = 'conv-h5' + path.basename.slice(6);
+            basename = basename.slice(0, -3);
+            path.basename = basename;
+            path.extname = packageConfig.version + '.min.js'
+        }))
+        .pipe(gulp.dest('./dist/'));
+});
+gulp.task('css', function(){
+    gulp.src([
+        './src/libs/Swiper/dist/css/swiper.min.css',
+        './src/libs/animate.css/animate.min.css'
+    ]).pipe(
+        gulpConcat('conv-h5.'+packageConfig.version+'.min.css')
+    ).pipe(gulp.dest('./dist/'));
+});
