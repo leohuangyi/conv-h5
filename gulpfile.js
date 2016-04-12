e5fe02bb84873fab8dc5914cf360a96f0b58ddc3
xitang.hy <xitang.hy@alibaba-inc.com>
2016-04-12 20:25:09 +0800

    移除错误Swiper

--- gulpfile.js
+++ gulpfile.js
@@ -9,8 +9,12 @@ gulp.task('build', ['clean', 'css', 'js']);
 gulp.task('clean', function(){
     del('./dist/*');
 });
+gulp.task('conv-h5-build', function(){
+    gulp.src('./src/libs/conv-h5.js')
+        .pipe();
+});
 gulp.task('js', function(){
-    gulp.src(['./src/libs/Swiper/dist/js/jquery.min.js'])
+    gulp.src(['./src/libs/Swiper/dist/js/Swiper.jquery.min.js'])
         .pipe(gRename(function(path){
             var basename = 'conv-h5' + path.basename.slice(6);
             basename = basename.slice(0, -3);
